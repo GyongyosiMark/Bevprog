47db3bc65ac46599aa007785ce1bb6f889c26f12
Kaiusz <55788794+Kaiusz@users.noreply.github.com>
2019-10-02 10:49:25 +0200

    Update drill4.1.cpp

--- drill4.1.cpp
+++ drill4.1.cpp
@@ -9,15 +9,9 @@ int main()
 a=1;
 b=1;
 
-while (a != 0)
-{cin >> c;
-if (c==0)
-      break;
-
-a=c;
-cin >> c;
-b=c;
-cout << a << " " << b; } cout << b;
+while (cin >> a && cin >>b)
+{
+cout << a << " " << b;
  if (a < b )
       {cout << "the smaller number is " << a << endl;
        cout << "the bigger number is " << b << endl;
@@ -29,7 +23,7 @@ if (a == b) {cout << "equal" << endl;}
 n=abs(a)- abs(b);
 p=1/100;
 if (n < p)
-{cout << "almost equal" << endl;
+{cout << "almost equal" << endl;}
 
 }
 }
